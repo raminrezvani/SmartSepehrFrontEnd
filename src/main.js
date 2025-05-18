60d9e3f832da1b6f9a63dd622508f00b9d05a000
ramin <raminrezvani@ymail.com>
2025-05-18 21:37:15 +0330

    chore: update axios base URL to production server
    The base URL for axios has been changed from the local development server to the production server at http://185.252.30.120:8000/. This change is necessary to ensure the application connects to the correct backend environment in production.

--- src/main.js
+++ src/main.js
@@ -13,7 +13,8 @@ const access_token = localStorage.getItem("access_token");
 axios.defaults.timeout = 1000 * 60 * 3;   // 2 minutes
 // axios.defaults.baseURL = "https://tour-collector-api.sepehrsmart.ir/";
 // axios.defaults.baseURL = "https://smarttravelback.iotstream.ir/";
-axios.defaults.baseURL = "http://127.0.0.1:8765/";
+// axios.defaults.baseURL = "http://127.0.0.1:8765/";
+axios.defaults.baseURL = "http://185.252.30.120:8000/";
 // 
 // axios.defaults.baseURL = "http://94.74.182.183:8585/";
 
